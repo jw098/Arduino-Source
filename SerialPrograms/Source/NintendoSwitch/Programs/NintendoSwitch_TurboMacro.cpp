bd6b0deee5cfe85a6b5910526f1a4a1af1d15084
jw098 <justin.wong098@gmail.com>
2026-02-05 22:27:30 -0800

    add run_until callback to Turbomacro

--- SerialPrograms/Source/NintendoSwitch/Programs/NintendoSwitch_TurboMacro.cpp
+++ SerialPrograms/Source/NintendoSwitch/Programs/NintendoSwitch_TurboMacro.cpp
@@ -5,7 +5,10 @@
  */
 
 #include "NintendoSwitch_TurboMacro.h"
-
+#include "CommonTools/Async/InferenceRoutines.h"
+#include "PokemonLA/Inference/Sounds/PokemonLA_ShinySoundDetector.h"
+#include "PokemonLZA/Options/PokemonLZA_ShinyDetectedAction.h"
+#include "NintendoSwitch/Programs/NintendoSwitch_GameEntry.h"
 //#include <iostream>
 //using std::cout;
 //using std::endl;
@@ -40,22 +43,70 @@ TurboMacro::TurboMacro()
             ControllerClass::NintendoSwitch_RightJoycon,
         }
     )
+    , RUN_UNTIL_CALLBACK(
+        "Trigger to stop program:",
+        {
+            {RunUntilCallback::NONE,         "none",           "None"},
+            {RunUntilCallback::LZA_SHINY_SOUND,            "lza-shiny-sound",              "LZA: Shiny Sound Detected"},
+        },
+        LockMode::LOCK_WHILE_RUNNING,
+        RunUntilCallback::NONE
+    )
 {
     PA_ADD_OPTION(LOOP);
+    PA_ADD_OPTION(RUN_UNTIL_CALLBACK);
     PA_ADD_OPTION(TABLE);
 }
 
 
 void TurboMacro::program(SingleSwitchProgramEnvironment& env, CancellableScope& scope){
+    switch (RUN_UNTIL_CALLBACK){
+    case RunUntilCallback::NONE:
+        run_table(env, scope);
+        break;
+    case RunUntilCallback::LZA_SHINY_SOUND:
+        run_table_stop_when_lza_shiny_sound(env, scope);
+        break;
+    default:
+        throw InternalProgramError(nullptr, PA_CURRENT_FUNCTION, "TurboMacro::program(): Unknown RunUntilCallback");
+        
+    }
+}
 
-    //  Connect the controller.
-    //pbf_press_button(context, BUTTON_LCLICK, 40ms, 40ms);
 
+void TurboMacro::run_table(SingleSwitchProgramEnvironment& env, CancellableScope& scope){
     for (uint32_t c = 0; c < LOOP; c++){
         TABLE.run(scope, env.console.controller());
     }
 }
 
+void TurboMacro::run_table_stop_when_lza_shiny_sound(SingleSwitchProgramEnvironment& env, CancellableScope& scope){
+    PokemonLZA::ShinySoundDetectedActionOption shiny_detected_option("Shiny Detected", "", "1000 ms", PokemonLZA::ShinySoundDetectedAction::NOTIFY_ON_FIRST_ONLY);
+    PokemonLZA::ShinySoundHandler shiny_sound_handler(shiny_detected_option);
+    PokemonLA::ShinySoundDetector shiny_detector(env.console, [&](float error_coefficient) -> bool {
+        //  Warning: This callback will be run from a different thread than this function.
+        // env.console.overlay().add_log("Shiny Sound Detected!", COLOR_YELLOW);
+        return shiny_sound_handler.on_shiny_sound(
+            env, env.console,
+            0,
+            error_coefficient
+        );
+    });
+
+    ProControllerContext context(scope, env.console.controller<ProController>());
+
+    int ret = run_until(
+        env.console, scope,
+        [&](CancellableScope& scope){
+            run_table(env, scope);
+        },
+        {shiny_detector}
+    );
+
+    if (ret == 0){
+        go_home(env.console, context);
+    }
+}
 
 }
 }
--- SerialPrograms/Source/NintendoSwitch/Programs/NintendoSwitch_TurboMacro.h
+++ SerialPrograms/Source/NintendoSwitch/Programs/NintendoSwitch_TurboMacro.h
@@ -27,10 +27,20 @@ public:
 
     virtual void program(SingleSwitchProgramEnvironment& env, CancellableScope& scope) override;
 
+    void run_table(SingleSwitchProgramEnvironment& env, CancellableScope& scope);
+
+    void run_table_stop_when_lza_shiny_sound(SingleSwitchProgramEnvironment& env, CancellableScope& scope);
+
 
 private:
     SimpleIntegerOption<uint32_t> LOOP;
     ControllerCommandTables TABLE;
+
+    enum class RunUntilCallback{
+        NONE,
+        LZA_SHINY_SOUND,
+    };
+    EnumDropdownOption<RunUntilCallback> RUN_UNTIL_CALLBACK;
 };
 
 
